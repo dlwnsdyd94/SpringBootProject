7c476d9c60e56866ddb4b4e15cbc2e40c19e51df
dlwnsdyd94 <ljy1237@yonsei.ac.kr>
2020-11-30 02:40:01 +0900

    JPA 연관관계 설정

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -2,10 +2,15 @@ package com.example.study.repository;
 
 import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.User;
-import org.junit.Test;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
@@ -17,26 +22,55 @@ public class UserRepositoryTest extends StudyApplicationTests {
     public void create(){
         // String sql = insert into user ( %s, %s, %d ) value (account, email, age);
         User user = new User();
-        user.setAccount("TestUser01");
-        user.setEmail("TestUser01@gmail.com");
-        user.setPhoneNumber("010-1111-1111");
+        user.setAccount("TestUser03");
+        user.setEmail("TestUser03@gmail.com");
+        user.setPhoneNumber("010-1111-3333");
         user.setCreatedAt(LocalDateTime.now());
-        user.setCreatedBy("admin");
+        user.setCreatedBy("TestUser3");
+        //System.out.println(("User : " + user));
 
         User newUser = userRepository.save(user);
-        System.out.println(("newUser : " +newUser));
+        System.out.println(("newUser : " + newUser));
     }
 
+    @Test
     public void read(){
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            System.out.println("user : "+selectUser);
+            System.out.println("email : "+selectUser.getEmail());
+        });
 
     }
 
+    @Test
+    @Transactional
     public void update(){
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser ->{
+            selectUser.setAccount("PPPP");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
     }
 
+    @Test
+    @Transactional
     public void delete(){
+        Optional<User> user = userRepository.findById(3L);
 
+        Assert.assertTrue(user.isPresent()); // true
+
+        user.ifPresent(selectUser->{
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(3L);
+
+        Assert.assertFalse(deleteUser.isPresent()); // false
     }
-
 }
